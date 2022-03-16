cb9b7b84e1df3f4b93ef933acb9c51542599d0e8
Dimitar Apostolovski <8737118+themetar@users.noreply.github.com>
2022-03-16 10:06:51 +0100

    Add theodinproject lesson examples in spec

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -19,3 +19,34 @@ describe 'from/to_index' do
     end    
   end 
 end
+
+describe 'knight_moves' do
+  xit 'handles zero steps' do
+    expect(knight_moves([0, 0], [0, 0])).to eq([[0, 0]])
+    expect(knight_moves([2, 3], [2, 3])).to eq([[2, 3]])
+  end
+
+  xit 'handles one step' do
+    expect(knight_moves([0, 0], [1, 2])). to eq([[0, 0], [1, 2]])
+  end
+
+  xit 'handles two steps' do
+    expect(knight_moves([0, 0], [3, 3])).to eq([[0, 0], [1, 2], [3, 3]]).or eq([[0, 0], [2, 1], [3, 3]])  # might have two shortest paths
+    expect(knight_moves([3, 3], [0, 0])).to eq([[3, 3], [1, 2], [0, 0]]).or eq([[3, 3], [2, 1], [0, 0]])
+  end
+
+  xit 'handles three steps' do
+    expect(knight_moves([3, 3], [4, 3])).to eq([[3, 3], [4, 5], [2, 4], [4, 3]])  # might have several shortest paths
+                                        .or eq([[3, 3], [1, 2], [2, 4], [4, 3]])
+                                        .or eq([[3, 3], [1, 4], [2, 2], [4, 3]])
+                                        .or eq([[3, 3], [4, 1], [2, 2], [4, 3]])
+                                        .or eq([[3, 3], [1, 2], [3, 1], [4, 3]])
+                                        .or eq([[3, 3], [5, 2], [3, 1], [4, 3]])
+                                        .or eq([[3, 3], [1, 4], [3, 5], [4, 3]])
+                                        .or eq([[3, 3], [5, 4], [3, 5], [4, 3]])
+                                        .or eq([[3, 3], [4, 1], [6, 2], [4, 3]])
+                                        .or eq([[3, 3], [5, 4], [6, 2], [4, 3]])
+                                        .or eq([[3, 3], [4, 5], [6, 4], [4, 3]])
+                                        .or eq([[3, 3], [5, 2], [6, 4], [4, 3]])
+  end
+end
